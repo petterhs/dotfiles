ba645ba4f16bbe8a6b5d619be7c0d56817afb231
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2025-09-21 17:01:56 +0200

    Fix issue with conflicting values for gc

--- modules/hosts/littleboy.nix
+++ modules/hosts/littleboy.nix
@@ -26,13 +26,6 @@
 
   # No desktop environment - headless server
 
-  # Garbage collection (more aggressive for homelab)
-  nix.gc = {
-    automatic = true;
-    dates = "weekly";
-    options = "--delete-older-than 7d";
-  };
-
   # Nix trusted users
   nix.extraOptions = ''
     trusted-users = root petter
