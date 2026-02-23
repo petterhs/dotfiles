5885c5f7d6661c4bac5aaa7602d1119ce83ccf85
Petter Hoem Sletsjøe <petter.hoem.sletsjoe@semcon.com>
2026-02-23 12:39:02 +0100

    Add syncthing to all hosts

--- modules/hosts/fatman.nix
+++ modules/hosts/fatman.nix
@@ -57,4 +57,16 @@
   nix.extraOptions = ''
     trusted-users = root petter petter-work
   '';
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "petter";
+    group = "users";
+    dataDir = "/home/petter/.local/share/syncthing";
+    configDir = "/home/petter/.config/syncthing";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8384 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
--- modules/hosts/no-kon-lx-016.nix
+++ modules/hosts/no-kon-lx-016.nix
@@ -42,4 +42,16 @@
   nix.extraOptions = ''
     trusted-users = root s27731
   '';
+
+  services.syncthing = {
+    enable = true;
+    openDefaultPorts = true;
+    user = "s27731";
+    group = "users";
+    dataDir = "/home/s27731/.local/share/syncthing";
+    configDir = "/home/s27731/.config/syncthing";
+  };
+
+  networking.firewall.allowedTCPPorts = [ 8384 ];
+  networking.firewall.allowedUDPPorts = [ 22000 21027 ];
 }
